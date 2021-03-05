2695e6bc63171ef42fe22e6d95b11953000c40ca
Suwon Lee <lsw7169@snu.ac.kr>
2021-03-05 21:12:11 +0900

    시간 표시기 업데이트 - 12/24시간 형식, 초 표시/미표시 여부 변경 기능 추가 - 시계 아이콘을 누르면 모드별로 순환.

--- src/clock.js
+++ src/clock.js
@@ -1,13 +1,88 @@
 const clock = document.querySelector(".clock");
+const AMPM = document.querySelector('.noon-indicator');
+const modeChangeBtn = document.querySelector(".indication-mode-change-button");
+
+// Clock display mode:
+// A-mode: hh:mm:ss (24 hours)
+// B-mode: hh:mm (24 hours)
+// C-mode: hh:mm:ss (12 hours)
+// D-mode: hh:mm (12 hours)
 
 function updateClock () {
+    // Get the time
     const format = (v) => {
         const s = "00" + v;
         return s.substr(s.length-2, 2);
     }
+    const format12 = (v) => {
+        let s = [];
+        if (Number(v) > 12) {
+            s = "00" + (v-12);
+            AMPM.innerText = 'PM';
+        } else {
+            s = "00" + v;
+            AMPM.innerText = 'PM';
+        }
+        return s.substr(s.length-2, 2);
+    }
     const d = new Date();
     const [h,m,s] = [d.getHours(),d.getMinutes(),d.getSeconds()];
-    document.querySelector(".clock").innerText = `${format(h)}:${format(m)}:${format(s)}`;
+    
+    // Check mode and set the clock
+    const currentMode = clock.getAttribute('mode');
+    let timeDisplay = [];
+    if (currentMode === 'A') {
+        timeDisplay =  `${format(h)}:${format(m)}:${format(s)}`;
+        AMPM.classList.remove('visible');
+        AMPM.classList.add('invisible');
+    } else if (currentMode === 'B') {
+        timeDisplay =  `${format(h)}:${format(m)}`;
+        AMPM.classList.remove('visible');
+        AMPM.classList.add('invisible');
+    } else if (currentMode === 'C') {
+        timeDisplay =  `${format12(h)}:${format(m)}:${format(s)}`;
+        AMPM.classList.add('visible');
+        AMPM.classList.remove('invisible');
+    } else if (currentMode === 'D') {
+        timeDisplay =  `${format12(h)}:${format(m)}`;
+        AMPM.classList.add('visible');
+        AMPM.classList.remove('invisible');
+    } else {
+        const timeDisplay =  `${format(h)}:${format(m)}`;
+    }
+    document.querySelector(".clock").innerText = timeDisplay;
 }
-updateClock();
-setInterval(updateClock,1000);
+
+function modeChangeHandle(e) {
+    const btn = e.target;
+    clock.classList.remove('visible');
+    clock.classList.add('invisible');
+    setTimeout([],500);
+    clock.classList.remove('invisible');
+    clock.classList.add('visible');
+    const currentMode = clock.getAttribute('mode');
+    if (currentMode === 'A') {
+        clock.setAttribute('mode','B');
+    } else if (currentMode === 'B') {
+        clock.setAttribute('mode','C');
+    } else if (currentMode === 'C') {
+        clock.setAttribute('mode','D');
+    } else if (currentMode === 'D') {
+        clock.setAttribute('mode','A');
+    } else {
+        clock.setAttribute('mode','A');
+    }
+    updateClock();
+    console.log('button clicked!')
+}
+
+
+
+function init() {
+    updateClock();
+    setInterval(updateClock,1000);
+    modeChangeBtn.addEventListener("click",modeChangeHandle);
+}
+
+
+init();
